efb041ffa6c1f4191396a13d3b52ab18812d8b36
MukhachevMaksin <itmukhachev@gmail.com>
2020-12-02 17:29:57 +0300

    second commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,14 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     public static final String URL = "jdbc:mysql://localhost:3306/testjdbc?serverTimezone=Europe/Moscow";
@@ -18,5 +24,30 @@ public class Util {
             e.printStackTrace();
         }
         return connection;
+    }
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Properties settings = new Properties();
+                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+                settings.put(Environment.URL, Util.URL);
+                settings.put(Environment.USER, Util.USERNAME);
+                settings.put(Environment.PASS, Util.PASSWORD);
+                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+                settings.put(Environment.HBM2DDL_AUTO, "update");
+
+                Configuration configuration = new Configuration();
+                configuration.setProperties(settings);
+                configuration.addAnnotatedClass(User.class);
+
+                sessionFactory = configuration.buildSessionFactory();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return sessionFactory;
     }
 }
